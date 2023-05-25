ae6d71c37573abc0c0af0c7f708627fb53837fa8
unknown <neerajarao2898@gmail.com>
2023-05-25 19:36:19 +0530

    index file modified

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom'
 import './index.css';
 import App from "./App.js"
 import { BrowserRouter as Router } from 'react-router-dom';
 // import serve from 'serve';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root');
+render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>  , root
 );
